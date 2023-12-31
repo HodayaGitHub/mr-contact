2fef4f832599044a21296f7e1e5d66689a9de237
Amir Yankolovich <amiryankolovich2000@gmail.com>
2023-12-31 19:06:52 +0200

    Add classes to the footer

--- src/cmps/Footer.jsx
+++ src/cmps/Footer.jsx
@@ -1,6 +1,6 @@
 export function Footer(){
   return(
-  <section className='footer'>
+  <section className='app-header full main-layout'>
     <h4>Mister Contact</h4>
     <p>All Rights Reserved to MisterContact</p>
     <h5> Hodaya 🐨 & Amir 🦝</h5>
