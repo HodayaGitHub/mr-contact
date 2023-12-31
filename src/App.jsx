c2b6800dc6fc67dc7701978af7ffe28228fb631c
HodayaGitHub <hodaya1812@gmail.com>
2023-12-31 18:30:10 +0200

    Add header cmp

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
 import { HomePage } from './pages/HomePage.jsx'
+import {AppHeader} from './cmps/AppHeader.jsx'
 
 
 export function App() {
 
   return (
     <>
-      <div>
-        welcome to mister contact!!!
-      </div>
-      <Router>
-        <Routes>
-          <Route element={<HomePage />} path="/" />
 
-        </Routes>
-      </Router>
+      <AppHeader />
+      <main>
+        <Router>
+          <Routes>
+            <Route element={<HomePage />} path="/" />
+          </Routes>
+        </Router>
+      </main>
 
     </>
   )
