8f0f4d85d4f67616251c9819c63ee151780ec76e
Amir Yankolovich <amiryankolovich2000@gmail.com>
2023-12-31 18:31:21 +0200

    Add scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './assets/style/main.scss'
 
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
 import { HomePage } from './pages/HomePage.jsx'
